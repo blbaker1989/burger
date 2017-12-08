96ec28fdcd52f82517b958f5cabb839845fc218b
BrookeLBaker <blbaker1989@gmail.com>
2017-12-08 09:37:34 -0500

    update schema

--- config/orm.js
+++ config/orm.js
@@ -33,46 +33,46 @@ var orm = {
 
 
   insertOne: function(burger_name, callback){
-
-    var date = new Date();
-    var timestamp = ''+ date.getFullYear() + '-';
-    var month = '' + (date.getMonth() + 1);
-
-      if(month.length == 1){
-        month = '0' + month;
-      }
-    timestamp += month + '-';
-    var day = '' + date.getDate();
-
-      if(day.length == 1){
-        day = '0' + day;
-      }
-    timestamp += day + ' ';
-    var hour = '' + date.getHours();
-
-      if(hour.length == 1){
-        hour = '0' + hour;
-      }
-    timestamp += hour + ':';
-    var minute = '' + date.getMinutes();
-
-      if(minute.length == 1){
-        minute = '0' + minute;
-      }
-    timestamp += minute + ':';
-    var second = '' + date.getSeconds();
-
-      if(second.length == 1){
-        second = '0' + second;
-      }
-    timestamp += second;
-
+    //
+    // var date = new Date();
+    // var timestamp = ''+ date.getFullYear() + '-';
+    // var month = '' + (date.getMonth() + 1);
+    //
+    //   if(month.length == 1){
+    //     month = '0' + month;
+    //   }
+    // timestamp += month + '-';
+    // var day = '' + date.getDate();
+    //
+    //   if(day.length == 1){
+    //     day = '0' + day;
+    //   }
+    // timestamp += day + ' ';
+    // var hour = '' + date.getHours();
+    //
+    //   if(hour.length == 1){
+    //     hour = '0' + hour;
+    //   }
+    // timestamp += hour + ':';
+    // var minute = '' + date.getMinutes();
+    //
+    //   if(minute.length == 1){
+    //     minute = '0' + minute;
+    //   }
+    // timestamp += minute + ':';
+    // var second = '' + date.getSeconds();
+    //
+    //   if(second.length == 1){
+    //     second = '0' + second;
+    //   }
+    // timestamp += second;
+    //
 
     // Run MySQL Query
     connection.query('INSERT INTO burgers SET ?', {
       burger_name: burger_name,
       eaten: false,
-      date: timestamp
+      // date: timestamp
     }, function (err, result) {
       if (err) throw err;
       callback(result);
@@ -95,5 +95,5 @@ var orm = {
 
 
 
-// Export 
+// Export
 module.exports = orm;
